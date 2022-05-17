be5bcc2817efda46f9b16af0391beee6307ec07d
roisd <Openthed00r>
2022-05-17 13:07:45 +0700

    menambah layout menu bottom navigation

--- app/src/main/java/com/example/moviefinder/SplashActivity.kt
+++ app/src/main/java/com/example/moviefinder/SplashActivity.kt
@@ -1,11 +1,25 @@
 package com.example.moviefinder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+import android.view.WindowManager
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_splash)
+
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
