ef61b2816e1e3823551d7f81d37fd5057d302c80
roisd <Openthed00r>
2022-07-24 17:06:27 +0700

    theme is as expected

--- app/src/main/java/com/admaja/mofin/ui/activity/SplashActivity.kt
+++ app/src/main/java/com/admaja/mofin/ui/activity/SplashActivity.kt
@@ -1,12 +1,27 @@
 package com.admaja.mofin.ui.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.admaja.mofin.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        supportActionBar?.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, delay)
+
+    }
+
+    companion object {
+        const val delay = 5000L
     }
 }
